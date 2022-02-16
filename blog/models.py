3c68e824d981cddb43a42970ca16bd7d1b1ed41a
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-02-16 21:26:35 +0000

    created the blog app's model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
+from pydoc import describe
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class AuthorModel(models.Model):
+    name = models.ForeignKey(User,on_delete=models.CASCADE) 
+    profile = models.ImageField()
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=50)
+
+class BlogModel(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    timestamp = models.TimeField(auto_now_add=True)
+    thumb = models.ImageField()
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    author = models.OneToOneField(AuthorModel,on_delete=models.CASCADE)
