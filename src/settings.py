18057b9d3fc4d36a43461b8205414b2beb700b11
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-02-17 21:20:31 +0000

    finished setting up media and static settings

--- src/settings.py
+++ src/settings.py
@@ -103,10 +103,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#   'static/',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
