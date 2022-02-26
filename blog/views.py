ab0b3a53328fcf3f5d4b720954f0e8458fa20a25
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-02-26 16:32:20 +0000

    added paginations to the blog list page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import PostModel
+# from django.core.paginator import Paginator, EmptyPage, PageNotInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def indexView(request):
@@ -13,9 +15,26 @@ def indexView(request):
     return render(request,'index.html',context)
 
 def listView(request):
-    posts = PostModel.objects.order_by('-timestamp')
+    post_list = PostModel.objects.all()
+    latest_posts = PostModel.objects.order_by('-timestamp')
+
+    paginator = Paginator(post_list,4)
+
+    # page_var = 'page'
+    page = request.GET.get('page',1)
+    try:
+        paginator_qs = paginator.page(page)
+
+    except PageNotAnInteger:
+        paginator_qs = paginator.page(1)
+
+    except EmptyPage:
+        paginator_qs = paginator.page(paginator.num_pages)
+
     context={
-        'posts': posts
+        # 'posts': posts
+        'posts': paginator_qs,
+        'latest':latest_posts
         }
     return render(request,'blog.html',context)
 
